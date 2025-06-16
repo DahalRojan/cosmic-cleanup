e1a10bfab03c15f89446b35a8f03280059b72bfa
DahalRojan <rojan.dahal14@gmail.com>
2025-06-15 20:54:29 -0400

    updating play controller for future work

--- Assets/Scripts/Controllers/CameraLookController.cs
+++ Assets/Scripts/Controllers/CameraLookController.cs
@@ -1,16 +1,16 @@
-using UnityEngine;
+// using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
-public class CameraLookController : MonoBehaviour
-{
-    public float sensitivity = 150f;
-    float yaw, pitch;
+// [RequireComponent(typeof(Camera))]
+// public class CameraLookController : MonoBehaviour
+// {
+//     public float sensitivity = 150f;
+//     float yaw, pitch;
 
-    void Update()
-    {
-        yaw   += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
-        pitch -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
-        pitch  = Mathf.Clamp(pitch, -89f, 89f);
-        transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
-    }
-}
+//     void Update()
+//     {
+//         yaw   += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+//         pitch -= Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+//         pitch  = Mathf.Clamp(pitch, -89f, 89f);
+//         transform.localRotation = Quaternion.Euler(pitch, yaw, 0);
+//     }
+// }
